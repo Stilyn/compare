766f2bbb2ace5416be9e5f4e5cc7e09dc4f31e62
stilyn76@gmail.com <[Desig76g_]>
2021-02-11 15:52:26 +0300

    боремся чо чравнением - ошибка индех out of range остальное вроде поека норм

--- main.py
+++ main.py
@@ -26,12 +26,21 @@ def delete_paragraph(paragraph):
     p.getparent().remove(p)
     p._p = p._element = None
 
+
 # функция удаление пустых строк из документа
 def strip_file(file, file_new):
     for paragraphs in file.paragraphs:
         if len(paragraphs.text) == 0:
             delete_paragraph(paragraphs)
     file.save(file_new)
+
+
+# функция добавления пустых абзацев в документ
+def add_par(document, par_count):
+    for f in range(par_count - 1):
+        document.add_paragraph('   ')
+        document.save('555.docx')  # подумать как назвать файл
+
 
 # функция сравнения блоков текста paragraph
 def f_compare(p1, p2):
@@ -43,6 +52,7 @@ def f_compare(p1, p2):
     else:
         sentences1 = nltk.sent_tokenize(p2)  # массив предложений 2
         sentences2 = nltk.sent_tokenize(p1)  # массив предложений 1
+    # print(sentences1,sentences2)
     res = list(set(sentences1) - set(sentences2))
     if len(res) > 0:
         # лучше не просто печатать а накапливать в массив
@@ -56,9 +66,12 @@ if __name__ == '__main__':
     doc1 = docx.Document(config.file1)  # линкуем первый файл как docx из конфигурационника
     doc2 = docx.Document(config.file2)  # линкуем второй файл как docx из конфигурационника
 
-    # убрать пустые строки из файлов
-    strip_file(doc1,'111.docx')  # убираем из файлов лишние строки и сохраняем под другими именами
+    # убрать пустые строки из файлов - это будут выровненные файлы для сравнения
+    strip_file(doc1, '111.docx')  # убираем из файлов лишние строки и сохраняем под другими именами
     strip_file(doc2, '222.docx')  # убираем из файлов лишние строки и сохраняем под другими именами
+
+    doc1 = docx.Document('111.docx')  # загружаем очищенные документы
+    doc2 = docx.Document('222.docx')
 
     '''
     выбрать какой из подрезанных длиннее чтобы потом всегда из большего вычитать меньшее
@@ -68,15 +81,16 @@ if __name__ == '__main__':
         ln = len(doc1.paragraphs)
         d1 = doc1.paragraphs
         d2 = doc2.paragraphs
+        # уравнять количество параграфов путем добавления пустых параграфов в нужный жокумент
+        add_par(doc2, (len(doc1.paragraphs) - len(doc2.paragraphs)))
     else:
         ln = len(doc2.paragraphs)
         d1 = doc2.paragraphs
         d2 = doc1.paragraphs
-
+        # уравнять количество параграфов путем добавления пустых параграфов в нужный жокумент
+        add_par(doc1, (len(doc2.paragraphs) - len(doc1.paragraphs)))
     # запустить цикл сравнения с наибольшей длиной
-    print(ln)  # количество абзацев в самом длинном документе
+    # print(ln)  # количество абзацев в самом длинном документе
+
     for i in range(ln):
         f_compare(d1[i].text, d2[i].text)  # сравниваем по параграфам
-        # а если добавили лишний раздел в какой то из документов, то все равно из большего вычитаем меньшее
-        # doc1.add_paragraph('ass'); # добавляем новый параграф
-        # doc1.save('111.docx') # сохраняем файл
