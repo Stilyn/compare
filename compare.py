8c91011143db96b824d6b4f5fd2b636eac82ddfe
stilyn76 <76_>
2021-03-12 12:09:11 +0300

    смысловое сравнение документов

--- compare.py
+++ compare.py
@@ -5,11 +5,12 @@
 python3 compare.py Основы.docx Основы2.docx
 '''
 
-import os
-import string
-# imports
-import sys
 
+import time
+start_time = time.time() # время выполнения
+import os
+# import string
+import sys
 import docx  # библиотека работа в word
 from diff_match_patch import diff_match_patch as diff_module  # для сравнения и раскраски по совету коллег
 from docx import Document
@@ -18,45 +19,35 @@ from fuzzywuzzy import fuzz
 from jinja2 import Environment, FileSystemLoader
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-
 import config
 
 # ********************************************   смысловой разбор и поиск ключевых слов
-# import pullenti_wrapper
-# from pullenti_wrapper.langs import (set_langs, RU, EN)
-# set_langs([RU, EN])
-# from pullenti_wrapper.processor import (Processor, DATE, GEO, ORGANIZATION, PERSON, MONEY, ADDRESS)
-# processor = Processor([DATE, GEO, ORGANIZATION, PERSON, MONEY, ADDRESS])
-
-import pullenti
+# import pullenti
 from pullenti.Sdk import Sdk
-
-from pullenti.ner.AnalysisResult import AnalysisResult
-from pullenti.ner.Analyzer import Analyzer
-from pullenti.ner.ExtOntology import ExtOntology
-from pullenti.ner.ExtOntologyItem import ExtOntologyItem
-from pullenti.ner.MetaToken import MetaToken
-from pullenti.ner.MorphCollection import MorphCollection
-from pullenti.ner.NumberSpellingType import NumberSpellingType
-from pullenti.ner.NumberToken import NumberToken
-from pullenti.ner.Processor import Processor
 from pullenti.ner.ProcessorService import ProcessorService
-from pullenti.ner.ProxyReferent import ProxyReferent
-from pullenti.ner.Referent import Referent
-from pullenti.ner.Slot import Slot
 from pullenti.ner.SourceOfAnalysis import SourceOfAnalysis
-from pullenti.ner.TextAnnotation import TextAnnotation
-from pullenti.ner.TextToken import TextToken
-from pullenti.ner.Token import Token
-
-from pullenti.ner.keyword import KeywordAnalyzer
-
+# from pullenti.ner.AnalysisResult import AnalysisResult
+# from pullenti.ner.Analyzer import Analyzer
+# from pullenti.ner.ExtOntology import ExtOntology
+# from pullenti.ner.ExtOntologyItem import ExtOntologyItem
+# from pullenti.ner.MetaToken import MetaToken
+# from pullenti.ner.MorphCollection import MorphCollection
+# from pullenti.ner.NumberSpellingType import NumberSpellingType
+# from pullenti.ner.NumberToken import NumberToken
+# from pullenti.ner.Processor import Processor
+# from pullenti.ner.ProxyReferent import ProxyReferent
+# from pullenti.ner.Referent import Referent
+# from pullenti.ner.Slot import Slot
+# from pullenti.ner.TextAnnotation import TextAnnotation
+# from pullenti.ner.TextToken import TextToken
+# from pullenti.ner.Token import Token
+# from pullenti.ner.keyword import KeywordAnalyzer
 # инициализируем в полном обеме
 Sdk.initialize_all()
+
 # sys.setrecursionlimit(config.recursion_limit)
 # sys.setrecursionlimit(100)
-print(sys.getrecursionlimit())
-
+# print(sys.getrecursionlimit())
 
 def find_keys(slots):
     mslots = []
@@ -89,12 +80,10 @@ def mind_generate(txt):
         ss = find_keys(match.slots)
         # если не str пробежаться рекурсией до руды
     # for match1 in result1.entities:
-        # ss.append(entity) #for match in result.walk():
-     #   ss1 = find_keys(match1.slots)
-
-    print('*** slots **', ss)
+    # ss.append(entity) #for match in result.walk():
+    #   ss1 = find_keys(match1.slots)
+    # print('*** slots **', ss)
     return ss  # возвращает словарь ключевых слов файла
-
 
 # ********************************************смысловой разбор и поиск ключевых слов
 
@@ -106,54 +95,57 @@ def file_rename(file_name):
 
 
 # функция удаление параграфа из документа
-def delete_paragraph(paragraph):
-    p = paragraph._element
-    p.getparent().remove(p)
-    p._p = p._element = None
+# def delete_paragraph(paragraph):
+#     p = paragraph._element
+#     p.getparent().remove(p)
+#     p._p = p._element = None
 
 
 # функция удаление пустых параграфов из документа
 def strip_file(file, file_new):
     for paragraphs in file.paragraphs:
         if len(paragraphs.text) == 0:
-            delete_paragraph(paragraphs)
+            # delete_paragraph(paragraphs)
+            p = paragraphs._element
+            p.getparent().remove(p)
+            p._p = p._element = None
     file.save(file_new)
 
 
 # функция добавления пустых абзацев в документ
-def add_par(document, par_count, new_name):
-    for f in range(par_count):
-        document.add_paragraph(' ')
-        document.save(new_name)  # подумать как назвать файл
+# def add_par(document, par_count, new_name):
+#     for f in range(par_count):
+#         document.add_paragraph(' ')
+#         document.save(new_name)  # подумать как назвать файл
 
 
 # функция разбивки русского текста на слова
-def tokenize_ru(file_text):
-    # firstly let's apply nltk tokenization
-    tokens = word_tokenize(file_text)
-
-    # let's delete punctuation symbols
-    tokens = [i for i in tokens if (i not in string.punctuation)]
-
-    # deleting stop_words
-    stop_words = stopwords.words('russian')
-    # stop_words = []
-    stop_words.extend(['что', 'это', 'так', 'вот', 'быть', 'как', 'в', '—', '–', 'к', 'на', '...'])
-    tokens = [i for i in tokens if (i not in stop_words)]
-
-    # cleaning words
-    tokens = [i.replace("«", "").replace("»", "") for i in tokens]
-
-    return tokens
+# def tokenize_ru(file_text):
+#     # firstly let's apply nltk tokenization
+#     tokens = word_tokenize(file_text)
+#
+#     # let's delete punctuation symbols
+#     tokens = [i for i in tokens if (i not in string.punctuation)]
+#
+#     # deleting stop_words
+#     stop_words = stopwords.words('russian')
+#     # stop_words = []
+#     stop_words.extend(['что', 'это', 'так', 'вот', 'быть', 'как', 'в', '—', '–', 'к', 'на', '...'])
+#     tokens = [i for i in tokens if (i not in stop_words)]
+#
+#     # cleaning words
+#     tokens = [i.replace("«", "").replace("»", "") for i in tokens]
+#
+#     return tokens
 
 
 # функция поиска смыслового совпадения параграфов глубина threshold
-def par_match(p1, p2, thresold):
-    dmp = diff_module()
-    diff_module.Match_Threshold = thresold
-    # diff_module.Match_Distance = 0
-    matches = dmp.match_main(p1, p2, 0)
-    return matches
+# def par_match(p1, p2, thresold):
+#     dmp = diff_module()
+#     diff_module.Match_Threshold = thresold
+#     # diff_module.Match_Distance = 0
+#     matches = dmp.match_main(p1, p2, 0)
+#     return matches
 
 
 # функция сравнения блоков текста paragraph
@@ -162,7 +154,6 @@ def f_compare(p1, p2):
     for k in config.symbols_clear:
         p1 = str.replace(p1, k, ' ')
         p2 = str.replace(p2, k, ' ')
-
     dmp = diff_module()
     diffs = dmp.diff_main(p1, p2)  # разница
     # dmp.diff_cleanupSemantic(diffs)
@@ -170,10 +161,10 @@ def f_compare(p1, p2):
     return dmp.diff_prettyHtml(diffs)
 
 
-def color_paragraph(paragraph):
-    # paragraphs.runs[s].font.color.rgb = RGBColor(0xff, 0x00, 0x00) # красный текст после нуля просто цвет html
-    # paragraphs.runs[s].font.bold = True # жирный шрифт
-    paragraph.style.font.highlight_color = WD_COLOR.YELLOW  # цвет выделения желтый
+# def color_paragraph(paragraph):
+#     # paragraphs.runs[s].font.color.rgb = RGBColor(0xff, 0x00, 0x00) # красный текст после нуля просто цвет html
+#     # paragraphs.runs[s].font.bold = True # жирный шрифт
+#     paragraph.style.font.highlight_color = WD_COLOR.YELLOW  # цвет выделения желтый
 
 
 '''для использования отладочного и боевого режимов'''
@@ -211,16 +202,6 @@ q4 = []  # ключевые слова документа 1
 q41 = []
 q5 = []  # ключевые слова документа 2
 q51 = []
-
-
-
-# print(len(doc1.paragraphs), len(doc2.paragraphs))
-
-# уравниваем количество параграфов в документах
-# if len(doc1.paragraphs) > len(doc2.paragraphs):
-#    add_par(doc2, (len(doc1.paragraphs) - len(doc2.paragraphs)), file_rename(file2))
-# if len(doc2.paragraphs) > len(doc1.paragraphs):
-#    add_par(doc1, (len(doc2.paragraphs) - len(doc1.paragraphs)), file_rename(file1))
 
 print(len(doc1.paragraphs), len(doc2.paragraphs))
 
@@ -276,7 +257,7 @@ with open(file_compare_name_d, 'w') as f2:
                 row_cells[0].text = str(q1[i])  # сразу добавляем абзац документа 1 в docx
                 row_cells[1].text = str(a)  # и для docx
                 row_cells[2].text = str(q2[j])  # потом неплохо было бы их раскрасить
-            else:
+            #else:
                 # добавлять абзацы в отдельный список
                 # этот отдельный список крыжить на предмет совпадения
 
@@ -285,20 +266,23 @@ with open(file_compare_name_d, 'w') as f2:
                 # q3.append(a)  # сразу добавляем для html
                 # row_cells[1].text = str(a)  # и для docx
                 # row_cells[2].text = config.no_paragraph   # потом неплохо было бы их раскрасить
-                continue
+                # continue
 doc3.save(file_compare_name_d)  # сохраняем файл docx
 f2.close()
 # print(len(q1), len(q2), len(q3))
+
 # создаем файл html с результаттми сравнения
-file_compare_name = file1.split('.')[0] + '_vs_' + file2.split('.')[0] + '.html'
+# file_compare_name = file1.split('.')[0] + '_vs_' + file2.split('.')[0] + '.html'
 # запись в файл
-curr_dir = os.path.dirname(os.path.abspath(__file__))  # через jinja указываем что шаблон находится в корне
-env = Environment(loader=FileSystemLoader(curr_dir))  # через jinja подгружаем шаблон из текущей папки
-template = env.get_template('template.html') # через jinja
-print(len(q1), len(q2), len(q3))
-print('*****Записываю html*******')
-with open(file_compare_name, "w", encoding='utf-8') as f:
-    f.write(template.render(file_name1=file_rename(file1), file_name2=file_rename(file2), q1=q11, q2=q21, q3=q3, q4=q41,
-                            q5=q51,
-                            len=len(q3)))
-f.close()
+# curr_dir = os.path.dirname(os.path.abspath(__file__))  # через jinja указываем что шаблон находится в корне
+# env = Environment(loader=FileSystemLoader(curr_dir))  # через jinja подгружаем шаблон из текущей папки
+# template = env.get_template('template.html')  # через jinja
+#print(len(q1), len(q2), len(q3))
+print('Похожих абзацев:' + str(len(q3)))
+# print('*****Записываю html*******')
+# with open(file_compare_name, "w", encoding='utf-8') as f:
+#     f.write(template.render(file_name1=file_rename(file1), file_name2=file_rename(file2), q1=q11, q2=q21, q3=q3, q4=q41,
+#                             q5=q51,
+#                             len=len(q3)))
+# f.close()
+print("Время выполнения--- %s seconds ---" % (time.time() - start_time))
