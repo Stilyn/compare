07609fd2317897c304abe41deea13e44feacd365
stilyn76 <76_>
2021-03-31 15:53:17 +0300

    неограниченное количество файлов

--- compare.py
+++ compare.py
@@ -55,224 +55,219 @@ Sdk.initialize_all()
 # sys.setrecursionlimit(100)
 # print(sys.getrecursionlimit())
 
-# класс сравнения 2 документов формата docx
-class CompareDocx:
+# функция генерации ключевых слов
+def mind_generate(txt):
+    ss = []
+    processor = ProcessorService.create_processor()  # результаты по основным встроенным процессорам pullenti
+    processor_key = ProcessorService.create_specific_processor('KEYWORD')
+    # for analysers in processor_key.analyzers:
+    #    print(analyzers)
+    result = processor_key.process(SourceOfAnalysis(txt))
+    result1 = processor.process(SourceOfAnalysis(txt))
+    # print(result, result1)
+    for match in result.entities: ss.append(str(match))
+    for match1 in result1.entities: ss.append(str(match1))
+    ss = list(set(ss))  # чистим от дублей
+    # print('*** slots **', ss)
+    return ss  # возвращает словарь ключевых слов файла
 
-    # функция генерации ключевых слов
-    def mind_generate(self, txt):
-        ss = []
-        processor = ProcessorService.create_processor()  # результаты по основным встроенным процессорам pullenti
-        processor_key = ProcessorService.create_specific_processor('KEYWORD')
-        # for analysers in processor_key.analyzers:
-        #    print(analyzers)
-        result = processor_key.process(SourceOfAnalysis(txt))
-        result1 = processor.process(SourceOfAnalysis(txt))
-        # print(result, result1)
-        for match in result.entities: ss.append(str(match))
-        for match1 in result1.entities: ss.append(str(match1))
-        ss = list(set(ss))  # чистим от дублей
-        # print('*** slots **', ss)
-        return ss  # возвращает словарь ключевых слов файла
+# ********************************************смысловой разбор и поиск ключевых слов
 
-    # ********************************************смысловой разбор и поиск ключевых слов
+# функция переименования файлов для формирования временных
+def file_rename(file_name):
+    n = file_name.split('.')[0] + '_vs' + '.docx'
+    return str(n)
 
-    # функция переименования файлов для формирования временных
-    def file_rename(self, file_name):
-        n = file_name.split('.')[0] + '_vs' + '.docx'
-        return str(n)
+# функция удаление пустых параграфов из документа
+def strip_file(file, file_new):
+    for paragraphs in file.paragraphs:
+        if len(paragraphs.text) == 0:
+            # delete_paragraph(paragraphs)
+            p = paragraphs._element
+            p.getparent().remove(p)
+            p._p = p._element = None
+    file.save(file_new)
 
-    # функция удаление пустых параграфов из документа
-    def strip_file(self, file, file_new):
-        for paragraphs in file.paragraphs:
-            if len(paragraphs.text) == 0:
-                # delete_paragraph(paragraphs)
-                p = paragraphs._element
-                p.getparent().remove(p)
-                p._p = p._element = None
-        file.save(file_new)
+# функция добавления пустых абзацев в документ
+# def add_par(document, par_count, new_name):
+#     for f in range(par_count):
+#         document.add_paragraph(' ')
+#         document.save(new_name)  # подумать как назвать файл
 
-    # функция добавления пустых абзацев в документ
-    # def add_par(document, par_count, new_name):
-    #     for f in range(par_count):
-    #         document.add_paragraph(' ')
-    #         document.save(new_name)  # подумать как назвать файл
+# функция разбивки русского текста на слова
+# def tokenize_ru(file_text):
+#     # firstly let's apply nltk tokenization
+#     tokens = word_tokenize(file_text)
+#
+#     # let's delete punctuation symbols
+#     tokens = [i for i in tokens if (i not in string.punctuation)]
+#
+#     # deleting stop_words
+#     stop_words = stopwords.words('russian')
+#     # stop_words = []
+#     stop_words.extend(['что', 'это', 'так', 'вот', 'быть', 'как', 'в', '—', '–', 'к', 'на', '...'])
+#     tokens = [i for i in tokens if (i not in stop_words)]
+#
+#     # cleaning words
+#     tokens = [i.replace("«", "").replace("»", "") for i in tokens]
+#
+#     return tokens
 
-    # функция разбивки русского текста на слова
-    # def tokenize_ru(file_text):
-    #     # firstly let's apply nltk tokenization
-    #     tokens = word_tokenize(file_text)
+# функция поиска смыслового совпадения параграфов глубина threshold
+# def par_match(p1, p2, thresold):
+#     dmp = diff_module()
+#     diff_module.Match_Threshold = thresold
+#     # diff_module.Match_Distance = 0
+#     matches = dmp.match_main(p1, p2, 0)
+#     return matches
+
+# функция сравнения блоков текста paragraph
+def f_compare(p1, p2):
+    # чистим абзацы от шлака
+    for k in config.symbols_clear:
+        p1 = str.replace(p1, k, ' ')
+        p2 = str.replace(p2, k, ' ')
+    dmp = diff_module()
+    diffs = dmp.diff_main(p1, p2)  # разница
+    # dmp.diff_cleanupSemantic(diffs)
+    # вот здесь нужно значительно улучшить алгоритм сравнения
+    return dmp.diff_prettyHtml(diffs)
+
+# def color_paragraph(paragraph):
+#     # paragraphs.runs[s].font.color.rgb = RGBColor(0xff, 0x00, 0x00) # красный текст после нуля просто цвет html
+#     # paragraphs.runs[s].font.bold = True # жирный шрифт
+#     paragraph.style.font.highlight_color = WD_COLOR.YELLOW  # цвет выделения желтый
+
+# функция формирования датасета сравнения параграфов
+def par_compare(q1, q2, q4, q5, thresold):
+    # q1 q2  - тексты параграфов
+    # q4 q5  - ключевые слова параграфов
+    q21 = []  # для вывода совпадающих значений 1 и 2 документа
+    q2_2 = []  # для вывода несовпадающих значений из 2 документа
+    q3 = []  # процент совпадения
+    q51 = []
+    q5_2 = []  # для вывода несовпадающих значений ключей из 2 документа
+    result = []
+    for i in range(len(q1)):  # берем все параграфы документа 1
+        # q1 сразу выводим в таблицу
+        q21.append(' ')
+        q51.append(' ')
+        q3.append(' ')
+        for j in range(len(q2)):
+            q2_2.append(' ')
+            q5_2.append(' ')
+
+            # необходимо разбить на потоки
+            # th1 = Thread(target=fuzz.WRatio, args=(q1[i], q2[j]))
+            # th2 = Thread(target=fuzz.token_sort_ratio, args=(q4[i], q5[j]))
+            # th1.start()
+            # th2.start()
+            # th1.join()
+            # th2.join()
+
+            a = fuzz.WRatio(q1[i], q2[j])  # ищем совпадение по смыслу %
+            # a = fuzz.partial_token_sort_ratio(q1[i], q2[j])  # ищем совпадение по словам %
+            # print('% текст ********', a)
+            b = fuzz.token_sort_ratio(q4[i], q5[j])
+            # print('% ключи ********', b)
+            # if int(a) >= int(thresold) and int(b) >= int(thresold) and len(q4[i]) > 0 and len(q5[j]) > 0:
+            if a >= thresold and b >= thresold and len(q4[i]) > 0 and len(q5[j]) > 0:
+                # сначала все до равенства положить равным пустоте?
+                # print(i,j)
+                q3[i] = str(a) + '|' + str(b)  # или написать процент совпадения как среднее арифметическое из a b
+                q21[i] = q2[j]
+                q51[i] = q5[j]
+            else:  # наполняем мешок с несовпадениями
+                q2_2[j] = q2[j]
+                q5_2[j] = q5[j]
+            #   q21.append(' ')
+            #   q51.append(' ')
+
+    q21.extend(q2_2)
+    q51.extend(q5_2)
+
+    # очистить мешок с несовпадениями от пустых значений
+    while len(q21) > (len(q1) + len(q2)): del q21[-1]
+    while len(q51) > (len(q1) + len(q2)): del q51[-1]
     #
-    #     # let's delete punctuation symbols
-    #     tokens = [i for i in tokens if (i not in string.punctuation)]
+    # print(len(q1), len(q4), len(q3), len(q21), len(q51))
     #
-    #     # deleting stop_words
-    #     stop_words = stopwords.words('russian')
-    #     # stop_words = []
-    #     stop_words.extend(['что', 'это', 'так', 'вот', 'быть', 'как', 'в', '—', '–', 'к', 'на', '...'])
-    #     tokens = [i for i in tokens if (i not in stop_words)]
-    #
-    #     # cleaning words
-    #     tokens = [i.replace("«", "").replace("»", "") for i in tokens]
-    #
-    #     return tokens
+    # # print(len(list(set(q2_2))))
+    # выравниваем размерность перед формированием датасета
+    ln = max(len(q1), len(q4), len(q3), len(q21), len(q51))
+    mass = [q1, q4, q3, q21, q51]
+    for m in mass:
+        while len(m) < ln:
+            m.append(' ')
+        result.append(m)
+    return result
 
-    # функция поиска смыслового совпадения параграфов глубина threshold
-    # def par_match(p1, p2, thresold):
-    #     dmp = diff_module()
-    #     diff_module.Match_Threshold = thresold
-    #     # diff_module.Match_Distance = 0
-    #     matches = dmp.match_main(p1, p2, 0)
-    #     return matches
-
-    # функция сравнения блоков текста paragraph
-    def f_compare(self, p1, p2):
-        # чистим абзацы от шлака
-        for k in config.symbols_clear:
-            p1 = str.replace(p1, k, ' ')
-            p2 = str.replace(p2, k, ' ')
-        dmp = diff_module()
-        diffs = dmp.diff_main(p1, p2)  # разница
-        # dmp.diff_cleanupSemantic(diffs)
-        # вот здесь нужно значительно улучшить алгоритм сравнения
-        return dmp.diff_prettyHtml(diffs)
-
-    # def color_paragraph(paragraph):
-    #     # paragraphs.runs[s].font.color.rgb = RGBColor(0xff, 0x00, 0x00) # красный текст после нуля просто цвет html
-    #     # paragraphs.runs[s].font.bold = True # жирный шрифт
-    #     paragraph.style.font.highlight_color = WD_COLOR.YELLOW  # цвет выделения желтый
-
-    # функция формирования датасета сравнения параграфов
-    def par_compare(self, q1, q2, q4, q5, thresold):
-        # q1 q2  - тексты параграфов
-        # q4 q5  - ключевые слова параграфов
-        q21 = []  # для вывода совпадающих значений 1 и 2 документа
-        q2_2 = []  # для вывода несовпадающих значений из 2 документа
-        q3 = []  # процент совпадения
-        q51 = []
-        q5_2 = []  # для вывода несовпадающих значений ключей из 2 документа
-        result = []
-        for i in range(len(q1)):  # берем все параграфы документа 1
-            # q1 сразу выводим в таблицу
-            q21.append(' ')
-            q51.append(' ')
-            q3.append(' ')
-            for j in range(len(q2)):
-                q2_2.append(' ')
-                q5_2.append(' ')
-
-                # необходимо разбить на потоки
-                # th1 = Thread(target=fuzz.WRatio, args=(q1[i], q2[j]))
-                # th2 = Thread(target=fuzz.token_sort_ratio, args=(q4[i], q5[j]))
-                # th1.start()
-                # th2.start()
-                # th1.join()
-                # th2.join()
-
-                a = fuzz.WRatio(q1[i], q2[j])  # ищем совпадение по смыслу %
-                # a = fuzz.partial_token_sort_ratio(q1[i], q2[j])  # ищем совпадение по словам %
-                # print('% текст ********', a)
-                b = fuzz.token_sort_ratio(q4[i], q5[j])
-                # print('% ключи ********', b)
-                # if int(a) >= int(thresold) and int(b) >= int(thresold) and len(q4[i]) > 0 and len(q5[j]) > 0:
-                if a >= thresold and b >= thresold and len(q4[i]) > 0 and len(q5[j]) > 0:
-                    # сначала все до равенства положить равным пустоте?
-                    # print(i,j)
-                    q3[i] = str(a) + '|' + str(b)  # или написать процент совпадения как среднее арифметическое из a b
-                    q21[i] = q2[j]
-                    q51[i] = q5[j]
-                else:  # наполняем мешок с несовпадениями
-                    q2_2[j] = q2[j]
-                    q5_2[j] = q5[j]
-                #   q21.append(' ')
-                #   q51.append(' ')
-
-        q21.extend(q2_2)
-        q51.extend(q5_2)
-
-        # очистить мешок с несовпадениями от пустых значений
-        while len(q21) > (len(q1) + len(q2)): del q21[-1]
-        while len(q51) > (len(q1) + len(q2)): del q51[-1]
-        #
-        # print(len(q1), len(q4), len(q3), len(q21), len(q51))
-        #
-        # # print(len(list(set(q2_2))))
-        # выравниваем размерность перед формированием датасета
-        ln = max(len(q1), len(q4), len(q3), len(q21), len(q51))
-        mass = [q1, q4, q3, q21, q51]
-        for m in mass:
-            while len(m) < ln:
-                m.append(' ')
-            result.append(m)
-        return result
-
-    # функция разбиения документа формирования ключевых слов для каждого параграфа
-    def split_doc(self, paragraphs, list_text, list_keywords):
-        for g in paragraphs:  # заранее готовим списки ключевых слов и  тектсов параграфов для документа 1
-            g_mind = mind_generate(g.text)
-            list_text.append(g.text)
-            list_keywords.append(' '.join(g_mind))
+# функция разбиения документа формирования ключевых слов для каждого параграфа
+def split_doc(paragraphs, list_text, list_keywords):
+    for g in paragraphs:  # заранее готовим списки ключевых слов и  тектсов параграфов для документа 1
+        g_mind = mind_generate(g.text)
+        list_text.append(g.text)
+        list_keywords.append(' '.join(g_mind))
 
 
 '''для использования отладочного и боевого режимов'''
 
 if len(sys.argv) > 1:  # если из под командной строки запускаем
-    for i in range(len(sys.argv))
-    print('сравниваю ' + sys.argv[1] + ' и ' + sys.argv[2])
-    file1 = sys.argv[1]
-    file2 = sys.argv[2]
-    thresold = sys.argv[3]  # сделать вычислением последнего системного аргумента
+    files = []
+    for i in range(1,len(sys.argv)-1):
+        # print('сравниваю ' + sys.argv[1] + ' и ' + sys.argv[2])
+        files.append(sys.argv[i])
+    thresold = sys.argv[len(sys.argv)-1]  # сделать вычислением последнего системного аргумента
+    print(files, thresold)  # получаем список всех файлов для сравнения и глубину в %
+
 else:
     print('отладочный режим')  # если не из под командной строки запускаем
-    file1 = '906_2013.docx'
-    file2 = '64_2020.docx'
+    files = ['906_2013.docx', '64_2020.docx', '51_2014.docx']
     thresold = config.thresold
-
-doc1 = docx.Document(file1)  # линкуем первый файл как docx из конфигурационника
-doc2 = docx.Document(file2)  # линкуем второй файл как docx из конфигурационника
-
-# убрать пустые строки из файлов - это будут выровненные файлы для сравнения
-strip_file(doc1, file_rename(file1))  # убираем из файлов лишние строки и сохраняем под другими именами
-strip_file(doc2, file_rename(file2))  # убираем из файлов лишние строки и сохраняем под другими именами
-
-'''
-загружаем очищенные документы
-file_rename(file1) это имя переименованного файла 1
-file_rename(file2) это имя переименованного файла 2
-'''
-# doc1 = docx.Document(file_rename(file1))
-# doc2 = docx.Document(file_rename(file2))
 
 q1 = []  # очищенные списки для вывода 1 документа
 q2 = []  # очищенные списки для вывода 2 документа
 q4 = []  # ключевые слова документа 1
 q5 = []  # ключевые слова документа 2
 
-print(len(doc1.paragraphs), len(doc2.paragraphs))
+texts = []  # абзацы документа
+keywords = []  # ключевые слова абзацев документа
+files_vs = []  # список имен файлов сравнения
+for i in range(len(files)):
+    file_vs = file_rename(files[i])  # делаем временные файлы для сравнения
+    files[i] = docx.Document(files[i])  # линкуем файл  docx
+    strip_file(files[i], file_vs)  # убираем из файлов лишние строки и сохраняем под другими именами
+    files_vs.append(file_vs)
+    print(len(files[i].paragraphs))
+    print('***** Готовлю ключевые слова *******')
+    start_time_keys = time.time()  # время начала выполнения
+    # сделать через на multiprocessing сейчас на потоках
+    # th1 = Thread(target=split_doc, args=(doc1.paragraphs, q1, q4))  # поток 1
+    # th2 = Thread(target=split_doc, args=(doc2.paragraphs, q2, q5))  # поток 2
+    # th1.start()
+    # th2.start()
+    # th1.join()
+    # th2.join()
+    texts_ = []
+    keywords_ = []
+    split_doc(files[i].paragraphs, texts_, keywords_)
+    texts.append(texts_)
+    keywords.append(keywords_)
+    print("Время выполнения--- %s seconds ---" % (time.time() - start_time_keys) + '\n\n')
 
-print('***** Готовлю ключевые слова *******')
-start_time_keys = time.time()  # время начала выполнения
-# сделать через на multiprocessing сейчас на потоках
-# th1 = Thread(target=split_doc, args=(doc1.paragraphs, q1, q4))  # поток 1
-# th2 = Thread(target=split_doc, args=(doc2.paragraphs, q2, q5))  # поток 2
-# th1.start()
-# th2.start()
-# th1.join()
-# th2.join()
-split_doc(doc1.paragraphs, q1, q4)
-split_doc(doc2.paragraphs, q2, q5)
-
-print("Время выполнения--- %s seconds ---" % (time.time() - start_time_keys) + '\n\n')
+# print(len(keywords[0][0]),len(keywords[1][0]),len(keywords[2][0]))
 
 print('***** Сравниваю по смыслу, ключевым словам и готовлю сводную таблицу xlsx *******')
 start_time_compare = time.time()  # время начала выполнения
 file_compare_name_d = str(datetime.datetime.now()).replace(' ', '_').replace(':', '_').split('.')[0] + '.xlsx'
 file_compare_name_ht = str(datetime.datetime.now()).replace(' ', '_').replace(':', '_').split('.')[0] + '.html'
-# готовим словарь для записи
-comp = par_compare(q1, q2, q4, q5, thresold)  # сравниваем абзацы документа
+
+# готовим словарь для записи сравниваем первый файл со всеми остальными поочередно
+comp = par_compare(texts[0], texts[1], keywords[0], keywords[1], thresold)  # сравниваем абзацы документа
 # df = pd.DataFrame({file_rename(file1): comp[0], 'keywords1': comp[1],
 #                    '% смысл | % keys': comp[2],
 #                    file_rename(file2): comp[3], 'keywords2': comp[4]})
-df = pd.DataFrame({file_rename(file1): comp[0], '% смысл | % keys': comp[2], file_rename(file2): comp[3]})
+df = pd.DataFrame({files_vs[0]: comp[0], '% смысл | % keys': comp[2], files_vs[1]: comp[3]})
 print("Время выполнения--- %s seconds ---" % (time.time() - start_time_compare) + '\n\n')
 
 start_time_xlsx = time.time()
